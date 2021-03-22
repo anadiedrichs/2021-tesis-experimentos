1db963f6018d60c8a660a14042a0e0ad31162b08
Ana Laura Diedrichs <analauradiedrichs@gmail.com>
2021-03-22 14:05:59 -0300

    uso de dataset.R

--- 3-competidores-empiricos/util-competitors-calc.R
+++ 3-competidores-empiricos/util-competitors-calc.R
@@ -2,7 +2,7 @@ library(frost)
 library(caret)
 library(dplyr)
 library(here)
-
+source(here("utils","dataset.R"))
 # global variables
 
 
@@ -12,65 +12,6 @@ library(here)
 # 
 estaciones <- c("junin","tunuyan","agua_amarga","las_paredes","la_llave")
 
-get_dataset_for_regression <- function(station_name)
-{
-  dacc_daily_tmin <- read_csv("data/dacc-daily-tmin.csv", 
-                              col_types = cols(X1 = col_skip(), date = col_date(format = "%Y-%m-%d")))
-  
-  
-  if(is.null(station_name) | !(station_name %in% estaciones) ) stop("station_name must be a valid name from estaciones")
-  junin <- dacc_daily_tmin %>% 
-    select(-contains("radiacion_") & starts_with(station_name)) %>% 
-    rename_with( ~ tolower(gsub(paste0(station_name,"."), "", .x, fixed = TRUE))) 
-  # quita el junin. de los nombres de las columnas
-  
-  # Quiero predecir la temperatura mínima, si es o no es helada.
-  tmin <- junin %>% 
-    select(temp_min) 
-  colnames(tmin) <- "tmin"
-  # para regresion
-  data_tmin <- cbind(junin[1:(nrow(junin)-1),],tmin[2:nrow(tmin),])
-  return(data_tmin)
-}
-
-get_dataset_for_classification <- function(station_name)
-{ 
-  dacc_daily_tmin <- read_csv("data/dacc-daily-tmin.csv", 
-                              col_types = cols(X1 = col_skip(), date = col_date(format = "%Y-%m-%d")))
-  
-  if(is.null(station_name) | !(station_name %in% estaciones) ) stop("station_name must be a valid name from estaciones")
-  
-  junin <- dacc_daily_tmin %>% 
-    select(-contains("radiacion") & starts_with(station_name)) %>% 
-    rename_with( ~ tolower(gsub(paste0(station_name,"."), "", .x, fixed = TRUE))) 
-  # quita el junin. de los nombres de las columnas
-  
-  # Quiero predecir la temperatura mínima, si es o no es helada.
-  
-  tmin <- junin %>% 
-    select(temp_min) 
-  
-  colnames(tmin) <- "tmin"
-  
-  
-  tmin_helada <- tmin
-  
-  tmin_helada <- tmin_helada %>% mutate(tmin = case_when(
-    tmin <= 0 ~ as.character("helada"),  # frost event
-    TRUE ~ as.character("no-helada")   # no frost
-  ))
-  
-  # para regresion
-  # data_tmin <- cbind(junin[1:(nrow(junin)-1),],tmin[2:nrow(tmin),])
-  
-  data_clasificacion <- cbind(junin[1:(nrow(junin)-1),],tmin_helada[2:nrow(tmin_helada),])
-  
-  #Convierto a factor la columna tmin
-  
-  data_clasificacion$tmin <- as.factor(data_clasificacion$tmin)
-  
-  return(data_clasificacion)
-}
 # https://gist.githubusercontent.com/charly06/91578196fc615c5a79c7174318be4349/raw/d96a98c2933f5af141eac91af83c7895062d68a5/ggrocs.R
 ggrocs <- function(rocs, breaks = seq(0,1,0.1), legendTitel = "Leyenda") {
   if (length(rocs) == 0) {
